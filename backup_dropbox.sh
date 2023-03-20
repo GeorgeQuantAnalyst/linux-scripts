523233c61d8b352c59080d59aaa4528f709702df
George Quant Analyst <112611533+GeorgeQuantAnalyst@users.noreply.github.com>
2023-03-20 14:47:46 +0100

    Update backup_dropbox.sh

--- backup_dropbox.sh
+++ backup_dropbox.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -e
 
+#title           :backup_dropbox.sh
+#description     :Bash script backup Dropbox folder to timestamp zip file
+#author	         :George
+#date            :20220217
+#version         :1.0.0
+#usage		       :bash backup_dropbox.sh
+#bash_version    :5.0.17(1)-release
+#====================================================================================================================
+
 readonly time_stamp=$(date +"%Y%m%d %T")
 readonly zip_file_name=Dropbox_$(date +"%Y%m%d").zip
 
