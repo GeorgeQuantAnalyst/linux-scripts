23eb30f3ba26f498e7ef6435c14783661223ef22
George Quant Analyst <112611533+GeorgeQuantAnalyst@users.noreply.github.com>
2023-03-20 14:44:07 +0100

    Update compress_and_remove_old_rolling_logs_job.sh

--- compress_and_remove_old_rolling_logs_job.sh
+++ compress_and_remove_old_rolling_logs_job.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
+
+#title           :compress_and_remove_old_rolling_logs_job.sh
+#description     :Bash script compress and remove old rolling logs for jobs
+#author	         :George
+#date            :20230226
+#version         :1.0.0
+#usage		 :bash compress_and_remove_old_rolling_logs_job.sh
+#bash_version    :5.0.17(1)-release
+#====================================================================================================================
 
 APP_LOG_FOLDERS=("/home/prod1/log/bybit-place-trailing-stops-job-swing/" "/home/prod1/log/bybit-place-trailing-stops-job-position/")
-
 
 echo "Start job"
 for log_folder in "${APP_LOG_FOLDERS[@]}"; do
