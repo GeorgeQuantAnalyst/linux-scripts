15c66bf34aa321c10f7b3640fa0b4aa9df461620
George Quant Analyst <112611533+GeorgeQuantAnalyst@users.noreply.github.com>
2023-05-27 14:50:38 +0200

    Update compress_and_remove_old_rolling_logs_job.sh

--- compress_and_remove_old_rolling_logs_job.sh
+++ compress_and_remove_old_rolling_logs_job.sh
@@ -1,15 +1,26 @@
 #!/bin/bash -e
 
 #title           :compress_and_remove_old_rolling_logs_job.sh
-#description     :Bash script compress and remove old rolling logs for jobs
+#description     :Bash script compress and remove old rolling logs for trading bots
 #author	         :George
 #date            :20230226
-#version         :1.0.0
+#version         :1.0.1
 #usage		 :bash compress_and_remove_old_rolling_logs_job.sh
 #bash_version    :5.0.17(1)-release
 #====================================================================================================================
 
-APP_LOG_FOLDERS=("/home/prod1/log/bybit-place-trailing-stops-job-swing/" "/home/prod1/log/bybit-place-trailing-stops-job-position/")
+APP_LOG_FOLDERS=("/home/prod1/log/close-trades-at-time-bot-bybit-swing/" \
+"/home/prod1/log/close-trades-at-time-bot-bybit-intraday/" \
+"/home/prod1/log/close-trades-at-time-bot-bybit-position/" \
+"/home/prod1/log/forex-trend-screener-bot/" \
+"/home/prod1/log/crypto-trend-screener-bot/" \
+"/home/prod1/log/place-trailing-stops-bot-bybit-swing/" \
+"/home/prod1/log/place-trailing-stops-bot-bybit-intraday/" \
+"/home/prod1/log/place-trailing-stops-bot-bybit-position/" \
+"/home/prod1/log/early-reaction-bot-bybit-intraday/" \
+"/home/prod1/log/early-reaction-bot-bybit-position/" \
+"/home/prod1/log/early-reaction-bot-bybit-swing/")
+
 
 echo "Start job"
 for log_folder in "${APP_LOG_FOLDERS[@]}"; do
@@ -26,3 +37,4 @@ for log_folder in "${APP_LOG_FOLDERS[@]}"; do
         find . -name "*log.*" -type f -mtime +7 -delete
 done
 echo "Finished job"
+
