26067462b5d0e866604f76d3e29cabae524bbc2e
George Quant Analyst <112611533+GeorgeQuantAnalyst@users.noreply.github.com>
2023-06-04 19:50:06 +0200

    Update backup_dropbox.sh

--- backup_dropbox.sh
+++ backup_dropbox.sh
@@ -13,6 +13,6 @@ readonly time_stamp=$(date +"%Y%m%d %T")
 readonly zip_file_name=Dropbox_$(date +"%Y%m%d").zip
 
 echo "Start backup"
-rm ${zip_file_name}
+rm -rf ${zip_file_name}
 zip ${zip_file_name} -r  ~/Dropbox
 echo "Finished backup"
